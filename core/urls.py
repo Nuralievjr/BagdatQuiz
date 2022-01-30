2deba0533e70309bc391c078872a532d1e602ed0
ablainuraliev <ablainuraliev@gmail.com>
2022-01-30 19:12:48 +0600

    static

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('secret/', admin.site.urls),
     path('answer/', MyApi.as_view()),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
